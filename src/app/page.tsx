444fcdb78454df840bed2b2488a3de79142cee6c
mattdevelops <mhgarry92@gmail.com>
2024-08-05 22:10:59 -0400

    initial hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,62 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/maxWidthWrapper";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+import { GiAmpleDress } from "react-icons/gi";
+import { FaPaintBrush } from "react-icons/fa";
+import { MdOutlineBuild } from "react-icons/md";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { LiaDumpsterFireSolid } from "react-icons/lia";
+
+const perks = [
+  {
+    title: "Vintage Vibes",
+    Icon: GiAmpleDress,
+    description: "Find unique and one-of-a-kind items.",
+  },
+  {
+    title: "Support Artists",
+    Icon: FaPaintBrush,
+    description: "Support independent artists and creators.",
+  },
+  {
+    title: "DIY",
+    Icon: MdOutlineBuild,
+    description: "Find supplies and inspiration for your next project.",
+  },
+  {
+    title: "Handmade",
+    Icon: FaHandHoldingHeart,
+    description: "Find unique handmade items and unique genuine friendships.",
+  },
+  {
+    title: "Dumpster Treasure",
+    Icon: LiaDumpsterFireSolid,
+    description: "Find supplies and inspiration for your next project.",
+  },
+];
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-background">
-      <Button variant="outline">
-        <Image
-          src="/logo.png"
-          alt="Posh Punk Digital Flea Market"
-          width={24}
-          height={24}
-        />
-        <span className="p-2">Hello World</span>
-      </Button>
-    </div>
+    <header>
+      <MaxWidthWrapper>
+        <div className="py-8 mx-auto text-center flex flex-col bg-muted items-left px-8 max-w-7xl">
+          <h1 className="text-4xl font-bold-tracking-tight text-primary text-left sm:text-6xl mb-4">
+            Posh Punk .
+            <h2 className="font-bold text-foreground">Digital Flea Market</h2>
+          </h1>
+          <p className="mt-6 text-lg text-left max-w-prose text-primary-foreground">
+            A space for digital artists, collectors, DIYers, and makers to sell
+            their creations.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products">
+              <Button variant="default">
+                What Will Find You Today? &rarr;
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </header>
   );
 }
--- src/components/maxWidthWrapper.tsx
+++ src/components/maxWidthWrapper.tsx
@@ -1,1 +1,13 @@
-const maxWidthWrappe;
+import { cn } from "@/lib/utils";
+
+export default function MaxWidthWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={cn("max-w-7xl mx-auto px-4 sm:px-6 lg:px-8")}>
+      {children}
+    </div>
+  );
+}
