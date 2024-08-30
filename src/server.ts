260aaed99fdfee5c2443accf3dfd530beafcbc00
mattdevelops <mhgarry92@gmail.com>
2024-08-29 22:25:13 -0400

    serve nextjs app logic

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getPayloadClient } from './get-payload-client';
-import { nextHandler } from './next-utils';
+import { nextApp, nextHandler } from './next-utils';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -9,11 +9,19 @@ const start = async () => {
     const payload = await getPayloadClient({
         initOptions: {
             express: app, // telling payload to use express as app
-            onInit: async (cms) => {
-                cms.logger.info(`Payload is ready! 🚀 Admin Url: ${cms.getAdminURL()}`);  // log the admin url
+            onInit: async (cms ) => {
+                cms.logger.info(`Payload is ready! 🚀 Admin Url: ${cms.getAdminURL()}`);
             }
         },
     });
     app.use((req, res) => nextHandler(req, res)) // use the next handler to handle all requests directed to the app
+    nextApp.prepare().then(() => {
+        payload.logger.info(`Next.js app is ready! 🚀`);
+
+        app.listen(PORT, () => {
+            payload.logger.info(`PoshPunk Digital Flea Market is running on http://localhost:${PORT}`);
+        }) // start the express app on the specified port
+    }) // prepare the Next.js app to be served by the express app
 }
+
 start();
