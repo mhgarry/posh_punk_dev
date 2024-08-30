a55ba316cfd2d3d4003bf9f54e5f6d3ecfb16342
mattdevelops <mhgarry92@gmail.com>
2024-08-30 13:43:00 -0400

    give option to make admin role for dev

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,32 +1,31 @@
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig } from 'payload/types'
 
 export const Users: CollectionConfig = {
-  slug: 'users',
-    auth: true,
-    access: {
-      read: () => true,
-      create: () => true,
-    },
+	slug: 'users',
+	auth: true,
+	access: {
+		read: () => true,
+		create: () => true,
+	},
 
-
-
-    fields: [
-      {
-        "name": "role",
-        "label": "Role",
-        "type": "select",
-        "options": [
-          {
-            "label": "Admin",
-            "value": "admin"
-          },
-          {
-            "label": "User",
-            "value": "user"
-          }
-        ],
-      }
-    ]
-
+	fields: [
+		{
+			name: 'role',
+			label: 'Role',
+			admin: {
+				condition: ({ req }) => req.user.role === 'admin',
+			},
+			type: 'select',
+			options: [
+				{
+					label: 'Admin',
+					value: 'admin',
+				},
+				{
+					label: 'User',
+					value: 'user',
+				},
+			],
+		},
+	],
 }
-
--- src/server.ts
+++ src/server.ts
@@ -9,38 +9,37 @@ const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
 const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
-  req,
-  res
+	req,
+	res,
 })
 
 const startServer = async () => {
-  const payload = await getPayloadClient({
-    initOptions: {
-      express: app,
-      onInit: async (cms) => {
-        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
-      },
-    },
-  })
+	const payload = await getPayloadClient({
+		initOptions: {
+			express: app,
+			onInit: async (cms) => {
+				cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+			},
+		},
+	})
 
+	app.use(
+		'/api/trpc',
+		trpcExpress.createExpressMiddleware({
+			router: appRouter,
+			createContext,
+		}),
+	)
 
+	app.use((req, res) => nextHandler(req, res))
 
-  app.use( '/api/trpc', trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  }))
+	nextApp.prepare().then(() => {
+		payload.logger.info(`Next.js is ready `)
 
-  app.use((req, res) => nextHandler(req, res))
-
-  nextApp.prepare().then(() => {
-    payload.logger.info(`Next.js is ready `)
-
-    app.listen(PORT, async () => {
-      payload.logger.info(
-        `Payload server is listening on ${process.env.NEXT_API_URL}`,
-      )
-    })
-  })
+		app.listen(PORT, async () => {
+			payload.logger.info(`Payload server is listening on ${process.env.NEXT_API_URL}`)
+		})
+	})
 }
 
 startServer()
