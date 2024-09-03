be9d905d3bf7ffb0872fa23c9a40c55a92b5bc35
mattdevelops <mhgarry92@gmail.com>
2024-09-03 02:04:14 -0400

    email verification stuff

---
+++ src/components/verify-email.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const VerifyEmail = () => {
+	// const {} = trpc.auth
+}
+
+export default VerifyEmail
--- src/trpc/auth-rouer.ts
+++ src/trpc/auth-rouer.ts
@@ -1,7 +1,10 @@
+import { VerifyEmail } from '@/components/verify-email'
 import { AuthCredentialsValidator } from '../lib/validators/account-credentials-validator'
 import { publicProcedure, router } from './trpc'
 import { getPayloadClient } from '../get-payload'
 import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+import payload from 'payload'
 
 export const authRouter = router({
 	createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input }) => {
@@ -30,5 +33,23 @@ export const authRouter = router({
 		})
 
 		return { success: true, sentToEmail: email, message: 'User Created' }
+	}),
+
+	// create public procedure to verify registration VerifyEmail
+	verifyEmail: publicProcedure.input(z.object({ token: z.string() })).mutation(async ({ input }) => {
+		// the token is the input from the client
+		const { token } = input
+		// get payload client
+		const payload = await getPayloadClient()
+
+		const isVerified = await payload.verifyEmail({
+			collection: 'users',
+			token,
+		}) // verify email token and return boolean
+
+		// if token is not isVerified
+		if (!isVerified) throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+		return { success: true, message: 'Email Verified' }
 	}),
 })
