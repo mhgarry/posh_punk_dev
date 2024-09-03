5a91fef280a9c826f07d8191aedadaa6011de9bb
mattdevelops <mhgarry92@gmail.com>
2024-09-02 22:11:25 -0400

    add inital darkmode toggle

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,39 +2,6 @@
 
 import { CollectionConfig } from 'payload/types'
 
-// export const Users: CollectionConfig = {
-// 	slug: 'users',
-// 	auth: {
-// 		verify: {
-// 			generateEmailHTML: ({ token }) => {
-// 				return `
-// 				<div>
-// 					<h1>Verify your email</h1>
-// 					<p>Click the link below to verify your email address</p>
-// 					<a href="${process.env.NEXT_PUBLIC_DOMAIN}?token=${token}">Verify your email</a>
-// 				</div>
-// 			`
-// 			},
-// 		},
-// 	},
-// 	access: {
-// 		read: () => true,
-// 		create: () => true,
-// 	},
-// 	fields: [
-// 		{
-// 			name: 'role',
-// 			required: true,
-// 			defaultValue: 'user',
-// 			label: 'Role',
-// 			type: 'select',
-// 			options: [
-// 				{ label: 'Admin', value: 'admin' },
-// 				{ label: 'User', value: 'user' },
-// 			],
-// 		},
-// 	],
-// }
 export const Users: CollectionConfig = {
 	slug: 'users',
 	auth: {
